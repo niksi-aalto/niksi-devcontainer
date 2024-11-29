69b21594138705d7521665c7e4278360bba1cbba
Luukas Pörtfors <luukas.portfors@aalto.fi>
2024-11-29 12:40:58 +0200

    feat: allow empty paths

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
           imageDigest = "sha256:003d94e845d73ea81a9245edd90a4edf852ff19e5b9a7992692538ba6295a9be";
           sha256 = "sha256-WyoR/ALyynMeDTcfnc2iIpVSuHU1U8+NI+R8QVEDL4A=";
         },
-      paths,
+      paths ? [],
       pathsToLink ? ["/bin" "/usr"],
     }:
       pkgs.dockerTools.buildImage {
